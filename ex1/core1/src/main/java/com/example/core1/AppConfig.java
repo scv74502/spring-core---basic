d88b6adbbfbe2a0a3cfc4286dcd91ccc91778539
kunwoo park <scv74502@gmail.com>
2024-08-27 19:36:51 +0900

    appconfig refactoring

--- ex1/core1/src/main/java/com/example/core1/AppConfig.java
+++ ex1/core1/src/main/java/com/example/core1/AppConfig.java
@@ -1,8 +1,10 @@
 package com.example.core1;
 
+import com.example.core1.discount.DiscountPolicy;
 import com.example.core1.discount.FixDicountPolicy;
 import com.example.core1.discount.OrderService;
 import com.example.core1.discount.OrderServiceImpl;
+import com.example.core1.member.MemberRepository;
 import com.example.core1.member.MemberService;
 import com.example.core1.member.MemberServiceImpl;
 import com.example.core1.member.MemoryMemberRepository;
@@ -10,13 +12,21 @@ import com.example.core1.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDicountPolicy();
     }
 
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDicountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
     }
 }
